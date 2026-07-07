29012a76e971efccf23f3e32becb8c30edd861d0
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-07-07 13:52:57 +0200

    chore: bump version to 1.0.1

--- _meta.lua
+++ _meta.lua
@@ -3,5 +3,5 @@ return {
     name        = "nurikabe",
     fullname    = _("Nurikabe"),
     description = _("Paint the river: connect islands of numbered white cells."),
-    version     = "1.0.0",
+    version     = "1.0.1",
 }
