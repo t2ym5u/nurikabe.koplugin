6e5c3118ae1aa7a0c22ab54a482d387a29b0c03c
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-07-20 14:19:36 +0200

    i18n: add missing FR translations

---
+++ i18n_fr.lua
@@ -0,0 +1,4 @@
+return {
+    ["Nurikabe"] = { fr = "Nurikabe" },
+    ["Paint the river: connect islands of numbered white cells."] = { fr = "Peignez la rivière : reliez les îlots de cases blanches numérotées." },
+}
